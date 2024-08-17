7536555653b671be5720f9c59591e05004494c20
mithuchandrabiswas <mithubiswasinfo@gmail.com>
2024-08-17 15:29:36 +0600

    server added into mongodb

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.use(cookieParser());
 
 
 
+const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.2bu9h7l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
